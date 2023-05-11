0799ad64fb59cf59fac9faa6ea3ebfbca0234a1f
SmartDevSource <dreamrock@hotmail.fr>
2023-05-11 18:21:57 +0200

    Le commit qui sauve !

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   
   resources :users, only: [:show, :new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :likesgossip, only: [:new, :create, :destroy]
   resources :cities
 
   resources :gossips do
