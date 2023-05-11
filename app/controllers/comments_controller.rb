caee0f69408c83d72ed195fc42ff7e3f6b438242
SmartDevSource <dreamrock@hotmail.fr>
2023-05-11 10:28:43 +0200

    comments edit / destroy OK

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,12 +7,34 @@ class CommentsController < ApplicationController
   end
 
   def edit
+    @comment = Comment.find(params[:id])
   end
 
   def update
+    @comment = Comment.find_by(id: params[:id])
+    @path = Gossip.find_by(id: @comment.gossip_id)
+
+    if @comment.update(content: params[:comment][:content])
+      flash[:success] = "Le commentaire a été modifié."
+      redirect_to @path
+    else
+      Rails.logger.info(@comment.errors.inspect) 
+      render :edit
+    end
+
   end
 
   def destroy
+    @comment = Comment.find_by(id: params[:id])
+    @path = Gossip.find_by(id: @comment.gossip_id)
+
+    if @comment.destroy
+      flash[:success] = "Le commentaire a été supprimé."
+      redirect_to @path
+    else
+      Rails.logger.info(@comment.errors.inspect) 
+      render :edit
+    end
   end
 
 end
